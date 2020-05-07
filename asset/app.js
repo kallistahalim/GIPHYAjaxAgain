baea7e7698ce5aaf9cdd6bf5d8aefaf3ff939f82
kallistahalim <kallistahalim@gmail.com>
2020-05-07 00:23:03 -0700

    fix bug

--- asset/app.js
+++ asset/app.js
@@ -10,38 +10,40 @@ function generateButtons() {
         $("#button-here").append(button);
 
     }
+
+    $(".search-button").on("click", function () {
+        var name = $(this).data("name");
+        var queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + name + "&api_key=ckvCPO4jnCg338m2CttOtkDLb1bmZjK0&limit=10";
+    
+        $.ajax({
+                url: queryUrl,
+                method: "GET"
+            })
+            .done(function (response) {   
+                
+                for(var i = 0; i < response.data.length; i++) {
+                    console.log(response.data);
+                    var imageDiv = $("<div>");
+                    var image = $("<img>");
+                    var still = response.data[i].images.fixed_height_small_still.url;
+                    var animated = response.data[i].images.downsized_large.url;
+                    image.attr("src",still);
+                    image.attr("data-still", still);
+                    image.attr("data-animated", animated);
+                    image.attr("data-state", "still");
+                    image.attr("height", 100);
+                    image.addClass("image-class");
+                    imageDiv.append(image);
+                    $("#show-giphy-here").prepend(image);
+                }
+            })
+    });
+    
 }
 
 generateButtons();
 
 
-$(".search-button").on("click", function () {
-    var name = $(this).data("name");
-    var queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + name + "&api_key=ckvCPO4jnCg338m2CttOtkDLb1bmZjK0&limit=10";
-
-    $.ajax({
-            url: queryUrl,
-            method: "GET"
-        })
-        .done(function (response) {   
-            
-            for(var i = 0; i < response.data.length; i++) {
-                console.log(response.data);
-                var imageDiv = $("<div>");
-                var image = $("<img>");
-                var still = response.data[i].images.fixed_height_small_still.url;
-                var animated = response.data[i].images.downsized_large.url;
-                image.attr("src",still);
-                image.attr("data-still", still);
-                image.attr("data-animated", animated);
-                image.attr("data-state", "still");
-                image.attr("height", 100);
-                image.addClass("image-class");
-                imageDiv.append(image);
-                $("#show-giphy-here").prepend(image);
-            }
-        })
-});
 
 
 $(document).on("click", ".image-class", function() {
